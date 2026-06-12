f4afa4a0e97796086ea4d09fb1f8ce63f5abe0e6
GreenRoc114 <3310149631@qq.com>
2026-06-12 23:52:34 +0800

    feat: localise post-4 images, update metadata, and change favicon to cloud

--- src/config.ts
+++ src/config.ts
@@ -30,9 +30,7 @@ export const siteConfig: SiteConfig = {
   },
   favicon: [    // Leave this array empty to use the default favicon
     {
-      src: 'http://q1.qlogo.cn/g?b=qq&nk=3310149631@qq.com&s=640',    // Path of the favicon, relative to the /public directory
-    //   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
-    //   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
+      src: '/favicon/favicon-cloud.svg',    // Path of the favicon, relative to the /public directory
     }
   ]
 }
